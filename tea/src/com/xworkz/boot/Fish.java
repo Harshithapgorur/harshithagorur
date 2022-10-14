116a5a02003f9fb40ca2cc0c53a5e80f7e6a2978
unknown <harshithagorur@gmail.com>
2022-10-14 12:33:05 +0530

    hii

--- tea/src/com/xworkz/boot/Fish.java
+++ tea/src/com/xworkz/boot/Fish.java
@@ -3,75 +3,30 @@ package com.xworkz.boot;
 public class Fish {
 	public String name;
 	public String type;
-	public int    price;
-	public double weight;
-	public int    length;
+	public double price;
+	public float weight;
+	public double length;
 	
 	
-	public Fish(String name,int price)
-	{
-		 this.name=name;
-		 this.price=price;
-	    System.out.println("Checking the name and price of the fish");
+	public Fish() {
+		System.out.println("entered fish name");
+		
+	
+	}
+	public Fish(String name) {
+		this.name=name;
+		System.out.println("================");
 		
 	}
-	
-	public Fish(double weight,String type)
-	{
-	 this.weight=weight;
-	 this.type=type;
-		System.out.println("Checking the weight and type of fish");
+	public Fish(String name,double price) {
+		this.name=name;
+		this.price=price;
+		
 	}
-	
-	public Fish(int length,String name)
-	{
-		 this.length=length;
-		 this.name=name;
-		System.out.println("Checking the length and name of the fish");
-	}
-
-	public Fish(String type,double weight,int price)
-	{
-		 this.type=type;
-		 this.weight=weight;
-		 this.price=price;
-		System.out.println("Checking the type,weight and price of the Fish");
-	}
-	
-	public Fish(int price,String type,double weight)
-	{
-		 this.price=price;
-		 this.type=type;
-		this.weight=weight;
-		System.out.println("Checking the price,type and weight of the Fish");
-	}
-	
-	public Fish(int length,int price,String type)
-	{
+	public Fish(String name,double price,double length) {
+		this.name=name;
+		this.price=price;
 		this.length=length;
-	    this.price=price;
-	    this.type=type;
-		System.out.println("Checking the length,price,type of the Fish");
-		
 	}
-	
-	public Fish(double weight,int price,String type,int length)
-	{
-		 this.weight=weight;
-		 this.price=price;
-		 this.type=type; 
-		 this.length=length;
-		System.out.println("Checking the weight,price,type and length of the Fish");
-	}
-	
-	public Fish(int length,String type,int price,double weight)
-	{
-		 this.length=length;
-		 this.type=type;
-		 this.price=price;
-		 this.weight=weight;
-		System.out.println("Cheking the length,type,price and weight of the Fish");
-	}
-}
 
 }
