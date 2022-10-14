be7d8802f65ddd19aa1c5a91208d7886a700ff72
unknown <harshithagorur@gmail.com>
2022-10-14 16:29:34 +0530

    file

--- tea/src/com/xworkz/boot/Fish.java
+++ tea/src/com/xworkz/boot/Fish.java
@@ -28,5 +28,35 @@ public class Fish {
 		this.price=price;
 		this.length=length;
 	}
+public Fish(float weight,double price,double length) {
+	this.weight=weight;
+	this.price=price;
+	this.length=length;
+}
+
+public Fish(String type,float weight) {
+this.type=type;
+this.weight=weight;
+
 
 }
+public Fish(double length,double price) {
+	this.length=length;
+	this.price=price;
+	
+}
+
+public Fish(double length,float weight) {
+	this.length=length;
+	this.weight=weight;
+}
+
+public Fish(String type,float weight,double length) {
+	this.type=type;
+	this.weight=weight;
+		this.length=length;
+}
+
+}
+
+
--- tea/src/com/xworkz/boot/FishRunner.java
+++ tea/src/com/xworkz/boot/FishRunner.java
@@ -7,22 +7,37 @@ public class FishRunner {
 		Fish Fish1 =new Fish();//const, inta
 		System.out.println(Fish1.name);
 		System.out.println(Fish1.price);
+		System.out.println(Fish1.type);
+		System.out.println(Fish1.weight);
+		System.out.println(Fish1.length);
 		
-		Fish1.name="harshitha";
+	
+		Fish1.name="star Fish";
 		Fish1.price=100;
-		
 		System.out.println(Fish1.name);
 		System.out.println(Fish1.price);
-		
-		
-		
-		
-		
-		
-		
-		
+		System.out.println(Fish1.type);
+		System.out.println(Fish1.weight);
+		System.out.println(Fish1.length);
 		
 
+		Fish1.type="gold fish";
+		Fish1.weight=5;
+		System.out.println(Fish1.name);
+		System.out.println(Fish1.price);
+		System.out.println(Fish1.type);
+		System.out.println(Fish1.weight);
+		System.out.println(Fish1.length);
+		
+		Fish1.length=10;
+		Fish1.weight=5;
+		System.out.println(Fish1.name);
+		System.out.println(Fish1.price);
+		System.out.println(Fish1.type);
+		System.out.println(Fish1.weight);
+		System.out.println(Fish1.length);
+		
+	
 	}
-
-}
+	}
+	
